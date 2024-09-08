277ed4bb21ab9f05745fc7820883c8edc3546e01
Evgeniy Kanareikin <kenor@tut.by>
2024-09-08 12:04:37 +0300

    task17 implementation

--- src/main/java/by/clevertec/Main.java
+++ src/main/java/by/clevertec/Main.java
@@ -273,7 +273,11 @@ public class Main {
 
     public static void task17() {
         List<Student> students = Util.getStudents();
-//        students.stream() Продолжить ...
+        students.stream()
+                .map(Student::getGroup)
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
     }
 
     public static void task18() {
